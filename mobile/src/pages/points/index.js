46e19681debe8f976a371ed6fc5c6920630e598d
Derek Enrique Alves <59737946+derek-alves@users.noreply.github.com>
2020-07-31 23:00:42 -0300

    Mobile

---
+++ mobile/src/pages/points/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+const Points = () => {
+  return(
+    <View>
+      <Text>
+        Points
+      </Text>
+    </View>
+  )
+}
+
+export default Points;
